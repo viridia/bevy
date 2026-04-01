55b0e7b11b4b0cd30b7f09955ef2f48715171917
Talin <viridia@gmail.com>
2026-04-01 13:26:55 -0700

    New feathers widgets: * menu * listview * scrollbar * pane * subpane

---
+++ crates/bevy_feathers/src/containers/mod.rs
@@ -0,0 +1,8 @@
+//! Meta-module containing all feathers containers (passive widgets that hold other widgets).
+mod flex_spacer;
+mod pane;
+mod subpane;
+
+pub use flex_spacer::*;
+pub use pane::*;
+pub use subpane::*;
